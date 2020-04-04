777fdd19094eb8d438f81c73f3825800fc667b53
morti747 <Mrgt747@gmail.com>
2020-04-04 16:00:47 -0400

    Tutoriel complété

--- b000000000.py
+++ b000000000.py
@@ -5,6 +5,11 @@
 """
 
 eleves = {}
+def personne_elue(name):
+     return name == "Zureni"
+
+from collections import deque
+
 eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
 eleves["Amir"]=[]
 eleves["Franck"]=[]
@@ -20,11 +25,23 @@ eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
 
-
 def search(name):
+   visitees = []
    search_queue = deque()
    search_queue += eleves[name]
-   print( len(search_queue) )
+   print( len(eleves.values()) )
+   while search_queue:
+        personne = search_queue.popleft()
+        if not personne in visitees:
+           if personne_elue(personne):
+              print(personne + " a le fameux Mac")
+              return True
+           search_queue += eleves[personne]
+           visitees.append(personne)
    return False
+
+if __name__== "__main__":
+   search("Boris")
+
 
 
